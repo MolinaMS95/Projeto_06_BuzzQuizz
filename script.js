a075d575cbbcd2e76adf7b5d88e58a851565b73d
Matheus Molina <matheusmolina95@gmail.com>
2022-08-31 17:48:40 -0300

    Comportamento das respostas completo

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 const apiURL = "https://mock-api.driven.com.br/api/v4/buzzquizz/quizzes/10047";
 let quizzes;
+let questions = [];
+let hits = 0;
 
 //Atualmente pega apenas um quiz, o que está na URL
 //Mas será modificado pra pegar todos os quizzes no futuro
@@ -24,7 +26,6 @@ function displayQuizz(quizData){
     for(let i = 0; i<quizData.questions.length;i++){
         displayQuestion(quizData.questions[i], questionHolder, i);
     }
-    
 }
 
 function displayQuestion(data, holder, id){
@@ -46,14 +47,17 @@ function displayAnswers(data, id){
     const holder = document.querySelectorAll(".question")[id];
     const answerHolder = holder.querySelector(".answer-holder");
     let answers = shuffle(data);
+    let feedback = [];
     for(let i = 0; i<answers.length;i++){
         answerHolder.innerHTML += `
-            <li class="answer" id="${i}">
+            <li class="answer" onclick="selectAnswer(this)">
                 <img class="answer-image" src="${answers[i].image}" alt="answer illustration">
                 <p class="answer-text">${answers[i].text}</p>
             </li>
         `
+        feedback.push(answers[i].isCorrectAnswer);
     }
+    questions.push(feedback);
 }
 
 //Peguei do StackOverflow
@@ -74,3 +78,36 @@ function shuffle(originalArray) {
   }
 
 window.onload = getQuizzes;
+
+function selectAnswer(answer){
+    let question = answer.parentNode;
+    let questionNumber = question.parentNode.id;
+    let answerText = question.querySelectorAll('.answer-text');
+    for (let i = 0; i < question.children.length; i++){
+        if(question.children[i]!==answer){
+            question.children[i].classList.add('unselectedAnswer');
+            question.children[i].onclick = null;
+        }
+        if(questions[questionNumber][i] === true){
+            answerText[i].classList.add('correct');
+        }
+        else {
+            answerText[i].classList.add('wrong');
+        }
+    }
+    let isCorrectAnswer = answer.querySelector('.answer-text');
+    if(isCorrectAnswer.classList.contains('correct')){
+        hits++;
+    }
+    console.log(hits);
+    let nextQuestionNumber = Number(questionNumber) + 1;
+    let nextQuestion = document.getElementById(`${nextQuestionNumber}`);
+    if(nextQuestion !== null){
+        setTimeout(scrollNext, 2000, nextQuestion);
+    }
+
+}
+
+function scrollNext(element){
+    element.scrollIntoView();
+}
