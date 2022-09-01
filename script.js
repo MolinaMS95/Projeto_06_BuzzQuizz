9acb716e1ffeba2833eb81fced674df65cf6b0c7
Matheus Molina <matheusmolina95@gmail.com>
2022-09-01 16:19:53 -0300

    Classes CSS corrigidas e Layout da criação de quizz completo

--- script.js
+++ script.js
@@ -91,7 +91,7 @@ function selectAnswer(answer){
         let answerText = question.querySelectorAll('.answer-text');
         for (let i = 0; i < question.children.length; i++){
             if(question.children[i]!==answer){
-                question.children[i].classList.add('unselectedAnswer');
+                question.children[i].classList.add('unselected-answer');
             }
             if(questions[questionNumber][i] === true){
                 answerText[i].classList.add('correct');
@@ -135,10 +135,10 @@ function finishQuizz(){
             `<div class="level">
               <p>${hitPercent}% de acerto: ${quizzes.levels[i-1].title}</p>
             </div>
-            <div class="levelImg">
+            <div class="level-img">
               <img src="${quizzes.levels[i-1].image}" alt="level illustration"/>
             </div>
-            <div class="levelText">
+            <div class="level-text">
               <p>${quizzes.levels[i-1].text}</p>
             </div>`
         }
@@ -153,7 +153,7 @@ function restart(){
     hits = 0;
     const answers = document.querySelectorAll('.answer');
     for (let i = 0; i < answers.length; i++){
-        answers[i].classList.remove('unselectedAnswer');
+        answers[i].classList.remove('unselected-answer');
         answers[i].parentNode.classList.remove('answered');
         answers[i].lastElementChild.classList.remove('correct');
         answers[i].lastElementChild.classList.remove('wrong');
